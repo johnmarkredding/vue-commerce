5ef1c67837182349c5a065db2342d1fcaf64fd0e
John Mark <johomurk@gmail.com>
2023-07-01 11:15:52 +0000

    add support for fontawesome icons

--- vue-commerce/src/App.vue
+++ vue-commerce/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RegionSelector currency="GBP" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegionSelector from './components/RegionSelector.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RegionSelector
   }
 }
 </script>
--- vue-commerce/src/main.js
+++ vue-commerce/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faFlagUsa } from '@fortawesome/free-solid-svg-icons';
+
+const app = createApp(App);
+app
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app');
