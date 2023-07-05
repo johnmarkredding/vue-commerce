03e2961c84eadb5de480b7f1e78814c2d991aa0d
John Mark <johomurk@gmail.com>
2023-07-05 14:30:58 +0000

    Adjust card styling

--- vue-commerce/src/components/ProductListItem.vue
+++ vue-commerce/src/components/ProductListItem.vue
@@ -10,8 +10,8 @@ const cartStore = useCartStore();
   <li>
     <img alt="product header" :src="`${props.product.image}`" />
     <h3> {{ props.product.title }} </h3>
-    <div role="doc-subtitle"> {{ props.product.category }}</div>
-    <p>${{ props.product.getPriceString() }}</p>
+    <div role="doc-subtitle"><span> {{ props.product.category }} </span><span>${{ props.product.getPriceString() }}</span>
+    </div>
     <footer>
       <Button @click="(e) => { cartStore.add(props.product) }" icon="pi pi-shopping-cart" label="Add" severity="danger"
         aria-label="Add to cart" rounded />
@@ -26,8 +26,14 @@ li {
   list-style-type: none;
   max-width: 35rem;
   background-color: #fff;
-  border-radius: 3px;
   box-shadow: 0 1px 2px 1px #ddd;
+  padding: 1rem 1.5rem;
+  display: grid;
+  grid-template-areas:
+    "image"
+    "title"
+    "subtitle"
+    "footer";
 }
 
 li:hover {
@@ -39,15 +45,41 @@ img {
   max-height: 10rem;
   width: auto;
   height: auto;
+  grid-area: image;
 }
 
-Button:first-of-type {
-  margin-right: .5rem;
+h3 {
+  grid-area: title;
+  margin: 1rem 0 0.5rem 0;
+  text-align: left;
 }
 
-p {
-  line-height: 168%;
+div[role="doc-subtitle"] {
+  margin: 0.5rem 0 2rem 0;
+  grid-area: subtitle;
 }
 
-[role="doc-subtitle"] {}
+div[role="doc-subtitle"]>span {
+  float: left;
+  width: 50%;
+  display: block;
+  color: #888;
+}
+
+div[role="doc-subtitle"]>span:first-of-type {
+  text-align: left;
+}
+
+div[role="doc-subtitle"]>span:last-of-type {
+  text-align: right;
+}
+
+footer {
+  height: 100%;
+  grid-area: footer;
+  display: flex;
+  gap: 0.5rem;
+  justify-content: center;
+  align-items: flex-end;
+}
 </style>
